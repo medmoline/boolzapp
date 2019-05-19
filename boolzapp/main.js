f6cb1347d79425dcf7b6ae35f804f391999b7788
med_moline <antonio.medugno1990@gmail.com>
2019-05-19 22:01:28 +0200

    Restylist + aggiunta milestone 3 parte - B

--- boolzapp/main.js
+++ boolzapp/main.js
@@ -1,77 +1,97 @@
 // Milestone 1
 // Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 // Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde
+
 // Milestone 2
 // - Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
-// - Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+// - Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina
 
-$(document).ready(function() {
-  //quando clicco il bottone
+// //Milestone 3
+// - Click sul contatto mostra la conversazione del contatto cliccato, è possibile inserire nuovi messaggi per ogni conversazione
+// - Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+
+$(document).ready(function(){
   $('.send_text').click(function() {
     send_message();
     computer_response();
-  })
+  });
 
-  //quando clicco il tasto invio mando il messaggio e avrò la risposta del computer
-  $('.input_container input').keypress(function(event){
+  $('.send_container input').keypress(function(event) {
     if(event.which == 13){
       send_message();
       computer_response();
     }
   });
-  //pl
-  $('.search_container input').keyup(function(event){
+
+  $('.riquadro input').keyup(function() {
     //salvo il valore dell'input
     var search_name = $(this).val();
     //valore di reset per tornare al punto di partenza
-    $('.amico_container').show();
+    $('.contact').show();
     //se il valore è maggiore di zero
-      if (search_name.length > 0){
-        //a tutti gli h3
-        $('.amico_container .nome_amico h3').each(function(){
+    if (search_name.length > 0){
+      //a tutti gli span con classe .name
+      $('.name_container .name').each(function(){
         //se this non è incluso nel valore
-          if($(this).text().toLowerCase().includes(search_name.toLowerCase()) == false){
-            //con la funzione closest andiamo a prendere il padre di $(this) e non lo mostri
-            $(this).closest('.amico_container').hide();
-          }
-        })
-      }
-  });
-
-
-
-
-  //funzione per mandare messaggio
-  function send_message(){
-    var message = $('.input_container input').val();
-    //se il  messaggio è maggiore di zero
-    if(message.length > 0){
-      //faccio una copia del div nascosto
-      var copia = $('.message_container.template').clone();
-      //gli rimuovo la classe
-      copia.removeClass('template');
-      //setto l'html con il messaggio scritto nel div figlio del div template e aggiungo la classe my_text
-      copia.children('.message_template').html( message).addClass('my_text');
-      //appendo il tutto al contenitore reale dove verrà aggiunto il div creato
-      $('.real_message_container').append(copia);
-      //imposto l'input a zero
-      message = $('.input_container input').val('');
+        if($(this).text().toLowerCase().includes(search_name.toLowerCase()) == false){
+          //con la funzione closest andiamo a prendere il padre di $(this) e non lo mostri
+          $(this).closest('.contact').hide();
+        }
+      })
     }
+  })
 
+
+//funzione per invio dei messaggi
+  function send_message(){
+    //salvo il valore dell'elemento presente nell'input
+    var message = $('.send_container input').val();
+    console.log(message);
+    //faccio un clone del contenitori dei messaggi e gli tolgo la classe template
+    var copia = $('.message_container.template').clone().removeClass('template');
+
+    copia.children('.message_template').addClass('my_text').append('<span class="text">'+ message + '</span>').append('<span class="menu_container"><i class="fas fa-chevron-down"></i></span>').append('<div class="delete_menu invisible"><div class="info">Info Messaggio</div> <div class="delete">Cancella Messaggio</div></div>');
+
+    $('.real_message_container').append(copia);
   }
-
-  //funzione per la risposta del computer
+  //funzione per la risposta del computer dopo 1 secondo
   function computer_response(){
-    setTimeout(function(){
-      //creo una seconda copia del div nascostso
-       var copia_computer = $('.message_container.template').clone();
-       //Gli rimuovo la classe
-       copia_computer.removeClass('template');
-       //setto l'html con il messaggio scritto nel div figlio e gli aggiungo la classe apposita per il computer
-       copia_computer.children('.message_template').html('Ciao').addClass('computer_message');
-       //appendo tutto al container reale
-       $('.real_message_container').append(copia_computer);
-    }, 1000);//parirà 1 secondo dopo aver scritto il messaggio
+    setTimeout(function() {
+      //salvo il valore dell'elemento presente nell'input
+      var message = $('.send_container input').val();
+      console.log(message);
+      //faccio un clone del contenitori dei messaggi e gli tolgo la classe template
+      var copia = $('.message_container.template').clone().removeClass('template');
+
+      copia.children('.message_template').addClass('computer_message').append('<span class="text">Ok</span>').append('<span class="menu_container"><i class="fas fa-chevron-down"></i></span>').append('<div class="delete_menu invisible"><div class="info">Info Messaggio</div> <div class="delete">Cancella Messaggio</div></div>');
+
+      $('.real_message_container').append(copia);
+    },1000)
   }
+
+  //intercetto il click sul div contact
+      $('.contact').click(function(){
+  // recupero il nome del contatto cliccato
+      var contact_name = $(this).find('.contact_name .name_container h4').text();
+      console.log(contact_name);
+      // porto il nome del contatto cliccato nello spazio in alto, quello del contatto corrente
+      $('.name_container_current h4').text(contact_name);
+      // recupero l'immagine del contatto cliccato
+      var contact_img = $(this).find('.img_friend').children('img').attr('src');
+      // porto l'immagine del contatto cliccato nello spazio dell'immagine in alto, quella del contatto corrente
+      $('.img_friend_current').children('img').attr('src', contact_img);
+
+    });
+
+
+  $(document).on('click','.menu_container i', function(){
+    $(this).closest('.menu_container').siblings('.delete_menu').toggleClass('invisible');
+  });
+   $(document).on('click','.delete', function(){
+    $(this).closest('.message_container').children('.message_template').html('<p>Hai eliminato questo messaggio!!</p>');
+    $('.delete_menu').hide();
+   });
+
+
 
 })
